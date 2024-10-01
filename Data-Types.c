c62fedd3ef180c3af3157003ba687283a2c73ef0
Donny <mrbgone1982@hotmail.ca>
2024-10-01 12:00:12 -0500

    New simple project upload

---
+++ Data-Types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 42;
+    float number = 22.7;
+    char sign='#';
+    double value = 50.33;
+        printf("age is %d \n", age);
+        printf("number is %f \n", number);
+        printf("sign is %c \n", sign);
+        printf("double is %lf \n", value);
+    return 0;
+    }
